c13460ae52cddd2329b872f7f5e6f82abc58ced2
elad <elad@cloudents.com>
2021-04-26 09:24:38 +0300

    Adding TasksPage + setting App.js to render it

---
+++ src/pages/TasksPage/TasksPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './TasksPage.css';
+
+function TasksPage(props) {
+    return (
+        <div>
+            <h1>Todos</h1>
+            <input type="text" placeholder="What's next?"/>
+        </div>
+    );
+}
+
+export default TasksPage;
