33133bdeaba6809bc56029258de874dac451c258
elad <elad@cloudents.com>
2021-04-26 10:09:09 +0300

    Adding tasksList on TaskPage + adding TaskComponent

--- src/pages/TasksPage/TasksPage.js
+++ src/pages/TasksPage/TasksPage.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import './TasksPage.css';
 import { Container } from 'react-bootstrap';
+import TaskComponent from '../../components/TaskComponent/TaskComponent';
 
 function TasksPage(props) {
+    const [tasks, setTasks] = React.useState([]);
+
+    const tasksList = tasks.map((task,index) => <TaskComponent key={index} taskText={task}/>);
+
+    function handleClick(e) {
+        if (e.charCode === 13) {
+            addTask(e.target.value);
+            e.target.value = "";
+        }
+    }
+
+    function addTask(newTask) {
+        setTasks(tasks.concat(newTask));
+    }
+
     return (
         <Container className="p-tasks">
             <h1>Todos</h1>
-            <input type="text" placeholder="What's next?"/>
+            <input type="text" placeholder="What's next?" onKeyPress={handleClick}/>
+            {tasksList}
         </Container>
     );
 }
