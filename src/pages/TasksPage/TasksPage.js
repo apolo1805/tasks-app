cb82a54937429d1d1845fa21ab9c929c22a1dce6
elad <elad@cloudents.com>
2021-04-26 09:39:59 +0300

    Adding TasksPage content

--- src/pages/TasksPage/TasksPage.js
+++ src/pages/TasksPage/TasksPage.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './TasksPage.css';
+import { Container } from 'react-bootstrap';
 
 function TasksPage(props) {
     return (
-        <div>
+        <Container className="p-tasks">
             <h1>Todos</h1>
             <input type="text" placeholder="What's next?"/>
-        </div>
+        </Container>
     );
 }
 
